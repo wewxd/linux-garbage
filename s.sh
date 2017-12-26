47437926e23ba00748eaaf826751c28d5e8e0cb0
wewxd <victor.petitjean@viacesi.fr>
2017-12-26 10:47:28 +0000

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,15 +1,17 @@
+[[ -f ~/.bashrc ]] && . ~/.bashrc
+
 if [[ $(tty) == *"1" ]]; then
-	i=0
-	k=(Q S D F G H J K L M)
+	echo "      :)"
+	k=(Q S D F G H J K L M A Z E R T Y U I O P)
 	a=($(ls /usr/share/xsessions))
-	read -n1 -rsp "$(
-		for i in "${!a[@]}"; do
-			echo "${k[$i]}: ${a[$i]::-8}"
-		done
-	)" c
-	for i in "${!k[@]}"; do
+	for i in "${!a[@]}"; do
+		echo "${k[$i]}: ${a[$i]::-8}"
+	done
+	read -n1 -rsp 'anything else: none' c
+	for ((i=0;i<${#a[@]};i++)); do
 		if [[ ${k[$i]} == ${c^^} ]]; then
-		startx `cat "/usr/share/xsessions/${a[$i]}"|awk -F 'Exec=' '{print $2}'`
+			startx `cat "/usr/share/xsessions/${a[$i]}"|awk -F 'Exec=' '{print $2}'`
 		fi
 	done
+	echo ""
 fi
