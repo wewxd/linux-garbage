b533dbfad49adad38faa2edb97b8954a26bb38bc
wewxd <i@fuckmy.cat>
2018-02-19 11:35:49 +0000

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,15 +1,15 @@
 if [[ $(tty) == *'1' ]]; then
-	echo '      :)'
-	k=(Q S D F G H J K L M)
-	a=($(ls /usr/share/xsessions))
-	for i in "${!a[@]}"; do
-		echo "${k[$i]}: ${a[$i]::-8}"
-	done
-	read -n1 -rsp 'anything else: none' c
-	for ((i=0;i<${#a[@]};i++)); do
-		if [[ ${k[$i]} == ${c^^} ]]; then
-			startx `awk -F 'Exec=' '{print $2}' "/usr/share/xsessions/${a[$i]}"`
-		fi
-	done
-	echo ''
+    echo '      oui'
+    k=(Q S D F G H J K L M)
+    a=($(ls /usr/share/xsessions))
+    for i in "${!a[@]}"; do
+        echo "${k[$i]}: ${a[$i]::-8}"
+    done
+    read -n1 -rsp 'anything else: none' c
+    for ((i=0;i<${#a[@]};i++)); do
+        if [[ ${k[$i]} == ${c^^} ]]; then
+            startx `grep '^Exec=' "/usr/share/xsessions/${a[$i]}"|cut -d '=' -f2`
+        fi
+    done
+    echo ''
 fi
